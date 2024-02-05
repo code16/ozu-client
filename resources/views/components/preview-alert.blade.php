9f26223299319f2dd71bcadd3a8b7f8884216f6d
antoine <antoine.guingand@wanadoo.fr>
2024-02-05 18:46:54 +0100

    update deploy show url

--- resources/views/components/preview-alert.blade.php
+++ resources/views/components/preview-alert.blade.php
@@ -2,7 +2,7 @@
 @if(\Code16\JockoClient\Facades\Jocko::isPreview())
     <div style="background-color: #ffb; padding: .5rem; text-align: center; position: sticky; top: 0; z-index: 1000">
         Prévisualisation du site. Pour appliquer les changements, rendez-vous dans la
-        <a style="text-decoration: underline" href="{{ rtrim(config('jocko-client.api_host'), '/') }}/sharp/s-show/config" target="_blank">
+        <a style="text-decoration: underline" href="{{ rtrim(config('jocko-client.api_host'), '/') }}/sharp/s-show/deploy" target="_blank">
             page configuration
         </a>.
     </div>
