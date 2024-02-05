506ca10deafb8692a3037a490c822a8cc56788b2
antoine <antoine.guingand@wanadoo.fr>
2024-02-05 18:47:11 +0100

    update deploy show label

--- resources/views/components/preview-alert.blade.php
+++ resources/views/components/preview-alert.blade.php
@@ -3,7 +3,7 @@
     <div style="background-color: #ffb; padding: .5rem; text-align: center; position: sticky; top: 0; z-index: 1000">
         Prévisualisation du site. Pour appliquer les changements, rendez-vous dans la
         <a style="text-decoration: underline" href="{{ rtrim(config('jocko-client.api_host'), '/') }}/sharp/s-show/deploy" target="_blank">
-            page configuration
+            page publication
         </a>.
     </div>
 @endif
