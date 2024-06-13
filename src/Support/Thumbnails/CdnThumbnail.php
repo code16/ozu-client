690c3c994bbc1fce4b68b1b91e4943404eb36ab9
philippe <philippe@code16.fr>
2024-06-13 09:40:42 +0200

    Fix CdnThumbnail URLs to match new ImageKit API

--- src/Support/Thumbnails/CdnThumbnail.php
+++ src/Support/Thumbnails/CdnThumbnail.php
@@ -6,32 +6,36 @@ class CdnThumbnail extends Thumbnail
 {
     public function make(?int $width, ?int $height = null, bool $fit = false): ?string
     {
-        return sprintf(
-            '%s%s/storage/%s',
-            config('ozu-client.cdn_url'),
-            $this->generateUrlParameters($width, $height, $fit),
-            $this->mediaModel->file_name
-        );
+        if($cdnUrl = str(config('ozu-client.cdn_url'))->rtrim('/')) {
+            return sprintf(
+                '%s/storage/%s?%s',
+                $cdnUrl,
+                $this->mediaModel->file_name,
+                $this->generateUrlParameters($width, $height, $fit),
+            );
+        }
+
+        return null;
     }
 
     private function generateUrlParameters(?int $width, ?int $height, bool $fit): string
     {
         if (!$fit) {
             if ($width && $height) {
-                return sprintf('tr:w-%s,h-%s,c-at_max', $width, $height);
+                return sprintf('tr=w-%s,h-%s,c-at_max', $width, $height);
             }
-            if ($width) {
-                return sprintf('tr:w-%s', $width);
-            }
-            return sprintf('tr:h-%s', $height ?: 400);
+
+            return $width
+                ? sprintf('tr=w-%s', $width)
+                : sprintf('tr=h-%s', $height ?: 400);
         }
 
         if ($width && $height) {
-            return sprintf('tr:w-%s,h-%s,c-at_max', $width, $height);
+            return sprintf('tr=w-%s,h-%s,c-at_max', $width, $height);
         }
 
         $side = ($width ?: $height) ?: 400;
 
-        return sprintf('tr:w-%s,h-%s,c-maintain_ratio', $side, $side);
+        return sprintf('tr=w-%s,h-%s,c-maintain_ratio', $side, $side);
     }
 }
