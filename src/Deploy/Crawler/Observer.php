e272998fd9650b0c64b4b7cc1cce18c79e3abe3a
PatrickePatate <lucienpuget1@gmail.com>
2024-09-10 11:36:47 +0200

    Log 404 instead of crashing crawler when APP_DEBUG = true

--- src/Deploy/Crawler/Observer.php
+++ src/Deploy/Crawler/Observer.php
@@ -2,6 +2,7 @@
 
 namespace Code16\OzuClient\Deploy\Crawler;
 
+use Log;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\UriInterface;
 use Spatie\Export\Crawler\Observer as BaseObserver;
@@ -13,7 +14,14 @@ class Observer extends BaseObserver
         try {
             parent::crawled($url, $response, $foundOnUrl, $linkText);
         } catch(\RuntimeException $e) {
-            if(!app()->hasDebugModeEnabled() && preg_match('/returned status code \[4\d\d]/', $e->getMessage())) {
+            if(preg_match('/returned status code \[4\d\d]/', $e->getMessage())) {
+                Log::warning("Crawled URL {$url} found on {$foundOnUrl} returned status code 4xx", [
+                    'url' => (string) $url,
+                    'status_code' => $response->getStatusCode(),
+                    'found_on_url' => (string) $foundOnUrl,
+                    'link_text' => $linkText,
+                ]);
+
                 return;
             }
             throw $e;
